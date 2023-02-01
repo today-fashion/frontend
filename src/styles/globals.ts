399f0955d28107671fbd9a3d21e4fbdee837438d
th-release <th.release.cth@gmail.com>
2023-02-01 19:43:09 +0900

    style: remove comment

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'; // 글로벌 스타일 적용을 도와주는 styled-components내장 메서드
+import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { ThemeType } from './theme';
 
