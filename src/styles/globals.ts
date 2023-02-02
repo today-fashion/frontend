101d4c0a002dde8edebb29a0f0c9db568f312c85
th-release <th.release.cth@gmail.com>
2023-02-02 16:01:23 +0900

    remove: delete ThemeButton

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,7 +1,6 @@
-import { BtnStyleProps } from '@/utils/interfaces';
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
-import { activeBtn, darkTheme, defaultBtn, lightTheme, ThemeType } from './theme';
+import { ThemeType } from './theme';
 
 export const GlobalStyle = createGlobalStyle`
 	${reset}
@@ -26,19 +25,4 @@ export const GlobalStyle = createGlobalStyle`
     background-color: ${({ theme }: ThemeType) => theme.bgColor};
     color: ${({ theme }: ThemeType) => theme.fontColor}
   }
-`
-
-export const ThemeButton = styled.button `
-  margin: ${({ margin }: BtnStyleProps) => margin ? margin : "0"};
-  padding: ${({ padding }: BtnStyleProps) => padding ? padding : "0"};
-  background-color: ${({ themeColor, active }: BtnStyleProps) => active ? activeBtn.bgColor : themeColor ? defaultBtn.lightBgColor : defaultBtn.darkBgColor};
-  color: ${({ themeColor, active }: BtnStyleProps) => active ? activeBtn.fontColor : themeColor ? defaultBtn.lightFontColor : defaultBtn.darkFontColor};
-  border: ${({ border }: BtnStyleProps) => border ? lightTheme.borderColor : darkTheme.borderColor};
-  border-radious: ${({ borderRadius }: BtnStyleProps) => borderRadius ? borderRadius : "0"};
-  font-size: ${({ fontSize }: BtnStyleProps) => fontSize ? fontSize : "auto"};
-  font-weight: ${({ fontWeight }: BtnStyleProps) => fontWeight ? fontWeight : "auto"};
-  width: ${({ width }: BtnStyleProps) => width ? width : "auto"};
-  height: ${({ height }: BtnStyleProps) => height ? height : "auto"};
-  display: ${({ display }: BtnStyleProps) => display ? display : "block"};
-  {...any-button style};
 `
