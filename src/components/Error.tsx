6040f5c5e011436c7e68a2199ecb51600829bc1d
th-release <th.release.cth@gmail.com>
2023-02-02 15:59:27 +0900

    feat: add ThemeButton

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,14 +1,48 @@
+import { ThemeButton } from "@/components/ThemeButton"
 import { ErrorProps } from "@/utils/interfaces"
+import { useRouter } from "next/router"
 import styled from "styled-components"
 
-export const ServerError = ({ title, desc }: ErrorProps): JSX.Element => {
+export const ServerError = ({ title, desc, themeColor }: ErrorProps): JSX.Element => {
+  const router = useRouter()
   return (
     <ErrorPage>
       <Error>
         <ErrorLogo>Error!</ErrorLogo>
         <ErrorTitle>{title}</ErrorTitle>
         <ErrorDesc>{desc}</ErrorDesc>
-        
+        <ErrorBtns>
+          <ThemeButton 
+            themeColor={themeColor} 
+            active={false}
+            margin="0 4px"
+            fontSize={"18px"}
+            fontWeight={600}
+            width={"212px"}
+            height={"54px"}
+            display={"flex"}
+            borderRadius={"4px"}
+          >
+            이전 페이지
+          </ThemeButton>
+
+          <ThemeButton 
+            themeColor={themeColor} 
+            active={true}
+            margin="0 4px"
+            fontSize={"18px"}
+            fontWeight={600}
+            width={"212px"}
+            height={"54px"}
+            display={"flex"}
+            borderRadius={"4px"}
+          >
+            메인 페이지
+          </ThemeButton>
+        </ErrorBtns>
+        <ErrorBtnsMobile>
+
+        </ErrorBtnsMobile>
       </Error>
     </ErrorPage>
   )
@@ -39,6 +73,20 @@ const ErrorDesc = styled.p `
 `
 
 const ErrorLogo = styled.h1 `
-  font-size: 75px;
+  font-size: 80px;
+  font-style: italic;
 `
 
+const ErrorBtns = styled.div `
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media screen and (max-width: 480px) {
+    display: none;
+    align-items:flex-end;
+  }
+`
+
+const ErrorBtnsMobile = styled.div `
+
+`
