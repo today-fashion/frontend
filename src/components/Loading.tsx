db51d8e821ef7ff82ff796aae56a1c5530d5bee7
th-release <th.release.cth@gmail.com>
2023-02-02 10:05:58 +0900

    feat: add themeColor and Update Style

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,12 +2,14 @@ import { fadeIn, fadeOut } from '@/styles/fade';
 import { LoadProps } from '@/utils/interfaces';
 import styled from 'styled-components';
 
-export const Loading = ({ status }: LoadProps): JSX.Element => {
+export const Loading = ({ status, themeColor }: LoadProps): JSX.Element => {
   return (
-    <LoadingPage status={status}>
-      <i>
-        <TodayFashion>Today-Fasion</TodayFashion>
-      </i>
+    <LoadingPage status={status} themeColor={themeColor}>
+        <TodayFashion>
+          <i>
+            Today-Fasion
+          </i>
+        </TodayFashion>
     </LoadingPage>
   )
 }
@@ -16,12 +18,17 @@ const LoadingPage = styled.div `
   position: absolute;
   width: 100vw;
   height: 100vh;
-  z-index: 0;
+  z-index: 99999;
+  background-color: ${(props: LoadProps) => (props.themeColor ? "#fff" : "#242424")};
+  color: ${(props: LoadProps) => (props.themeColor ? "#242424" : "#fff")};
   animation ${(props: LoadProps) => (props.status ? fadeIn : fadeOut)} .75s forwards;
 `
 
 const TodayFashion = styled.h1 `
   position: absolute;
+  font-size: 24px;
+  font-weight: 600;
+  font-style: italic;
   text-align: center;
   left: 50%;
   top: 50%;
