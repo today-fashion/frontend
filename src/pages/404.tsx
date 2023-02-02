72441cb0a476677b433edc279105943e847265a2
th-release <th.release.cth@gmail.com>
2023-02-02 10:46:19 +0900

    feat: add themeColor

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,8 @@
 import { ServerError } from "@/components/Error"
+import { DefaultProps } from "@/utils/interfaces"
 
-export default function Error404 () {
+export default function Error404 ({ themeColor }: DefaultProps) {
   return (
-    <ServerError title="페이지를 찾을 수 없습니다." desc="주소가 잘못 입력되었거나, 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다." />
+    <ServerError title="페이지를 찾을 수 없습니다." desc="주소가 잘못 입력되었거나, 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다." themeColor={themeColor} />
   )
 }
