705ebe00bf233ca32e37b0e5f9504058f9aea565
th-release <th.release.cth@gmail.com>
2023-02-02 10:48:19 +0900

    style: add ,

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,13 +1,17 @@
 export const lightTheme = {
   bgColor: "#fff",
   fontColor: "#242424",
-  borderColor: "1px solid #eaeaea"
+  borderColor: "1px solid #eaeaea",
+  btnColor: "#0e2ee2",
+  btnFontColor: "#fff"
 }
 
 export const darkTheme = {
   bgColor: "#242424",
   fontColor: "#fff",
-  borderColor: "1px solid #2c2d33"
+  borderColor: "1px solid #2c2d33",
+  btnColor: "#0e2ee2",
+  btnFontColor: "#fff"
 }
 
 export const theme = {
