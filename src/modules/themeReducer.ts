11f953ff68b14e30c2ae698ba452dc5cba3446ce
th-release <th.release.cth@gmail.com>
2023-02-03 13:45:07 +0900

    feat: add setCookie

--- src/modules/themeReducer.ts
+++ src/modules/themeReducer.ts
@@ -1,16 +1,20 @@
+import { setCookie } from "@/utils/cookies";
 import { ThemeAction, ThemeState } from "@/utils/interfaces";
 
 export function themeReducer(state: ThemeState, action: ThemeAction): ThemeState {
   switch (action.type) {
     case "Light":
+      setCookie("Theme", "Light", { path: "/" });
       return {
         theme: true
       }
     case "Dark":
+      setCookie("Theme", "Dark", { path: "/" });
       return {
         theme: false
       }
     case "diff":
+      setCookie("Theme", state.theme ? "Dark" : "Light", { path: "/" });
       return {
         theme: !state.theme
       }
