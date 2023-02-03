53a05a2c9c6a664c74d40d52b950082b8f1fddf7
th-release <th.release.cth@gmail.com>
2023-02-03 15:16:41 +0900

    feat: add Loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,19 +26,30 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [])
   const { data, error } = useSWR('/api/userCheck', fetcher)
 
-  return (
+  if (!data) {
     <ThemeProvider theme={event.theme ? theme.lightTheme : theme.darkTheme}>
       <>
-        <GlobalStyle />
         <Head />
         <ThemeChanger themeColor={event.theme} onClick={() => updateEvent({ type: "diff" })} />
-        <Loading status={error ? false : !data ? true : false} themeColor={event.theme} />
-        {!error && data && data.body.success ?
-          <Component {...pageProps} themeColor={event.theme} /> 
-        :
-          <ServerError title='서버가 응답하지 않습니다.' desc='인터넷 상태를 확인해주세요!' themeColor={event.theme} />
-        } 
+        <Loading status={true} themeColor={event.theme} />
       </>
     </ThemeProvider>
-  )
+  } else {
+    return (
+      <ThemeProvider theme={event.theme ? theme.lightTheme : theme.darkTheme}>
+        <>
+          <GlobalStyle />
+          <Head />
+          <ThemeChanger themeColor={event.theme} onClick={() => updateEvent({ type: "diff" })} />
+          <Loading status={false} themeColor={event.theme} />
+          {!error && data && data.body.success ?
+            <Component {...pageProps} themeColor={event.theme} /> 
+          :
+            <ServerError title='서버가 응답하지 않습니다.' desc='인터넷 상태를 확인해주세요!' themeColor={event.theme} />
+          } 
+        </>
+      </ThemeProvider>
+    )
+  }
+
 }
