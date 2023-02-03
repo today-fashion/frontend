7942c4295ea6f4c8e2c55479af6b5a0e0a5477c5
th-release <th.release.cth@gmail.com>
2023-02-03 13:47:32 +0900

    style: rename props

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import { ServerError } from "@/components/Error"
-import { DefaultProps } from "@/utils/interfaces"
+import { Props404 } from "@/utils/interfaces"
 
-export default function Error404 ({ themeColor }: DefaultProps) {
+export default function Error404 ({ themeColor }: Props404) {
   return (
     <ServerError title="페이지를 찾을 수 없습니다." desc="주소가 잘못 입력되었거나, 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다." themeColor={themeColor} />
   )
