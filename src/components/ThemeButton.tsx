7493ce4d36cc759e1041a9fcf3545a2ce06dbe69
th-release <th.release.cth@gmail.com>
2023-02-03 09:08:15 +0900

    feat: use styledComponents, add onClick Event

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,28 +1,32 @@
 import { activeBtn, darkTheme, defaultBtn, lightTheme } from "@/styles/theme";
 import { BtnInterface } from "@/utils/interfaces";
+import styled from "styled-components";
 
 export function ThemeButton(props: BtnInterface): JSX.Element {
   return (
-    <button style={{ 
+    <Button style={{ 
       margin: props.margin ? props.margin : "0",
-      padding: props.padding ? props.padding : "0",
       backgroundColor: props.active ? activeBtn.bgColor : (props.themeColor ? defaultBtn.lightBgColor : defaultBtn.darkBgColor),
       color: props.active ? activeBtn.fontColor : (props.themeColor ? defaultBtn.lightFontColor : defaultBtn.darkFontColor),
       border: props.border ? props.border : (props.themeColor ? lightTheme.borderColor : darkTheme.borderColor),
-      borderRadius: props.borderRadius ? props.borderRadius : "0",
       fontSize: props.fontSize ? props.fontSize : "auto",
       fontWeight: props.fontWeight ? props.fontWeight : "auto",
       width: props.width ? props.width : "auto",
-      height: props.height ? props.height : "auto",
-      display: props.display ? props.display : "auto",
-      textAlign: "center",
-      alignItems: "center",
-      justifyContent: "center",
-      cursor: "pointer"
+      height: props.height ? props.height : "auto"
     }}
-      onClick={() => props.onClick}
+      onClick={props.onClick}
     >
       {props.children}
-    </button>
+    </Button>
   )
 }
+
+const Button = styled.button `
+  padding: 0;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  border-radius: 4px;
+  display: flex;
+`
